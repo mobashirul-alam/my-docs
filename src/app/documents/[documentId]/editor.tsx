a673f6205896c356da4586d82851ea0182715586
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-12-17 13:16:00 +0600

    Line height custom component added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
 import { useEditorStore } from "@/store/use-editor-store";
 import { Color } from "@tiptap/extension-color";
 import FontFamily from "@tiptap/extension-font-family";
@@ -84,6 +85,7 @@ export const Editor = () => {
                 types: ["heading", "paragraph"],
             }),
             FontSizeExtension, // custom extension
+            LineHeightExtension, // custom extension
         ],
         content: "<p>Hello World! 🌎️</p>",
     });
