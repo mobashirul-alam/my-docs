cbdac6b941f911bebc005d1c2ed2b497b6b79772
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-09-15 21:55:38 +0600

    Wrap SearchInput component in Suspense for improved loading handling

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
@@ -16,7 +17,9 @@ export const Navbar = () => {
                 </Link>
                 <h3 className="text-xl font-medium">My Docs</h3>
             </div>
-            <SearchInput />
+            <Suspense fallback={<div className="flex-1" />}>
+                <SearchInput />
+            </Suspense>
             <div></div>
         </nav>
     );
