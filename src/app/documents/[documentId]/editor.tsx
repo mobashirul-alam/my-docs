a957a4773ef01f5178eebd3d097b38bc25a36e0e
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-09-15 09:16:25 +0600

    Add Margin Ruler component and integrate with Editor; include react-icons dependency

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,11 +20,13 @@ import Underline from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import ImageResize from "tiptap-extension-resize-image";
+import { Ruler } from "./ruler";
 
 export const Editor = () => {
     const { setEditor } = useEditorStore();
 
     const editor = useEditor({
+        immediatelyRender: false,
         onCreate({ editor }) {
             setEditor(editor);
         },
@@ -92,6 +94,7 @@ export const Editor = () => {
 
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler />
             <div className="min-w-max flex justify-center w-[816px] px-4 py-4 print:py-0 mx-auto print:w-full print:min-h-0">
                 <EditorContent editor={editor} />
             </div>
