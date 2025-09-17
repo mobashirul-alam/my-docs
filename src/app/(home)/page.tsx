d2ab63cd4723a9d1190ce68bce38383e646f624d
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-09-17 20:16:49 +0600

    feat: implement document creation and documents table

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import { DocumentsTable } from "./documents-table";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
-    const documents = useQuery(api.documents.get);
-
-    if (!documents) {
-        return <div>Loading...</div>;
-    }
+    const { results, status, loadMore } = usePaginatedQuery(
+        api.documents.get,
+        {},
+        { initialNumItems: 5 }
+    );
 
     return (
         <div className="min-h-screen flex flex-col">
@@ -19,9 +20,11 @@ const Home = () => {
             </div>
             <div className="mt-16">
                 <TemplatesGallery />
-                {documents?.map((document) => (
-                    <span key={document._id}>{document.title}</span>
-                ))}
+                <DocumentsTable
+                    documents={results}
+                    status={status}
+                    loadMore={loadMore}
+                />
             </div>
         </div>
     );
